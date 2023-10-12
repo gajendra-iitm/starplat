fb2b40e1e75c279253728df8aa4f836bab72f651
Viswesh <visweshsuresh@gmail.com>
2023-10-12 14:45:49 +0530

    FPU

--- SSSP.py
+++ SSSP.py
@@ -1,5 +1,5 @@
 from math import inf
-from constructs import FixedPoint
+from constructs import FixedPointUntil, FixedPoint
 
 def Compute_SSSP(g, src_node):
     g.attachNodeProperty(distance=inf, modified=False, modified_next=False)
@@ -7,25 +7,35 @@ def Compute_SSSP(g, src_node):
     g.distance[src_node] = 0
 
     finished = False
+    
+    def custom_condition():
+        return finished
+    
+    with FixedPointUntil(custom_condition) as loop:
+        
+        def custom_block():
 
-    FixedPoint('''
-for v in filter(lambda node: g.modified[node] == True, g.nodes()):
+            for v in filter(lambda node: g.modified[node] == True, g.nodes()):
 
-    # Changed loop to accessing via nodes
-    for nbr in g.getOutNeighbors(v):
+                # Changed loop to accessing via nodes
+                for nbr in g.getOutNeighbors(v):
 
-        e = g.get_edge(v, nbr)
+                    e = g.get_edge(v, nbr)
 
-        new_distance = g.distance[v] + e.weight
-        if new_distance < g.distance[nbr]:
-               
-            g.distance[nbr] = new_distance
-            g.modified_next[nbr] = True
+                    new_distance = g.distance[v] + e.weight
+                    if new_distance < g.distance[nbr]:
+                        
+                        g.distance[nbr] = new_distance
+                        g.modified_next[nbr] = True
 
-# Making a deep copy
-g.modified = g.modified_next.copy()
+            # Making a deep copy
+            g.modified = g.modified_next.copy()
 
-g.attachNodeProperty(modified_next=False)
-        ''', g, finished)
-
+            g.attachNodeProperty(modified_next=False)
+            
+            
+            
+        
+        loop.run(custom_block)
+        
     return g.distance
