621b9b05b7243557b158fbd344dc6945b9fb5e4d
Viswesh <visweshsuresh@gmail.com>
2023-10-16 19:53:48 +0530

    FPU Construct

--- TriangleCounting.py
+++ TriangleCounting.py
@@ -2,10 +2,8 @@ def Compute_TC(g):
     triangle_count = 0
     
     for v in g.nodes():
-        lesser = list(filter(lambda u: u<v, g.neighbors(v)))
-        for u in lesser:
-            greater = list(filter(lambda w: w>v, g.neighbors(v)))
-            for w in greater:
+        for u in list(filter(lambda u: u<v, g.neighbors(v))):
+            for w in list(filter(lambda w: w>v, g.neighbors(v))):
                 if g.is_an_edge(u,w):
                     triangle_count += 1
                     
