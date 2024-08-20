71d153c6a1ce7934cb8c5eb9ddc45266c8759d30
anubhavkhajuria <anubhavkhajuria5@gmail.com>
2024-08-20 10:25:45 +0530

    updated forward Prop

--- graphcode/generated_omp/test11.cc
+++ graphcode/generated_omp/test11.cc
@@ -1,6 +1,6 @@
 #include "test11.h"
 
-void test1(graph &g, float *features, int *labels)
+void test1(graph &g, GNN &gnn, std::vector<vector<float>> features, int *labels)
 {
   //   int num_epoch = 100;
   //   int num_layer = 5;
@@ -39,25 +39,12 @@ void test1(graph &g, float *features, int *labels)
 
 int main()
 {
-  graph G("/Users/s.tharun_dyanish/Documents/vscode/StarPlat_Updated/graphcode/sample_graphs/sample_graph.txt");
+  graph G("../sample_graphs/sample_graph.txt");
 
   G.parseGraph();
-  // bool* modified_add ;
-  // bool* modified_del;
 
-  // // int triangleC =  Compute_TC(G);
-  // int elements = 0;
-
-  // for(int nod = 0; nod < G.num_nodes(); nod ++)
-  // {
-  //   for(auto edge: G.getEdges()[nod])
-  //   {
-  //     cout<<edge.source<<" "<<edge.destination<<endl;
-  //   }
-  // }
-
-  GNN gnn(G, "", "");
-  gnn.gcn_preprocessing();
+  GNN gnn(G, "../sample_graphs/sample_graph_features/sample_features.txt", "../sample_graphs/sample_graph_labels/sample_labels");
+  gnn.gcnPreprocessing();
 
   // print all the edges with weights
   for (int nod = 0; nod < G.num_nodes(); nod++)
@@ -69,4 +56,53 @@ int main()
   }
 
   printf("%s\n", environment.get_backend());
+
+  std::vector<int32_t> neuronsPerHiddenLayer;
+  neuronsPerHiddenLayer.push_back(8);
+  neuronsPerHiddenLayer.push_back(2);
+  // void GNN::initializeLayers(std::vector<int> neuronsPerLayer, char *initType)
+  gnn.initializeLayers(neuronsPerHiddenLayer, "xaviers");
+  std::vector<layer> layers = gnn.getLayers();
+
+  graph &g = gnn.getGraph();
+    for(int j = 0; j < gnn.getGraph().num_nodes(); j++)
+    {
+      for(int i = 0; i < gnn.numFeatures(); i++){
+      std::cout << layers[0].inputFeatures[j][i] << " ";
+      }
+    std::cout << std::endl;
+  }
+  //std::cout <<  "This is a partitioning beetween aggregated features and forward propagation"<< std::endl;
+  
+  gnn.forwardPass(0, 0);
+  int num_nodes = g.num_nodes();
+  for(int i = 0;i< layers[1].num_features;i++){
+    std::cout << layers[0].outputFeatures[0][i] << " ";
+  }
+  // for (int j = 0; j < num_nodes; j++)
+  // {
+  //   //layers[0].outputFeatures[0][j] = 0;
+  //   for (int i = 0; i < layers[0].num_features; i++)
+  //   {
+  //     std::cout<<layers[0].outputFeatures[0][j]<<" ";
+  //   }
+  //   std::cout<<std::endl;
+  //   // layers[0].outputFeatures[0][j] = relu(layers[0].outputFeatures[0][j] + layers[0].bias[j]);
+  // }
+
+  // for (int i = 0; i < layers.size() - 1; i++)
+  // {
+  //   cout << layers[i].num_features << endl;
+  //   // print weights
+  //   for (int j = 0; j < layers[i].num_features; j++)
+  //   {
+  //     for (int k = 0; k < layers[i + 1].num_features; k++)
+  //     {
+  //       cout << i << "$" << j << "$" << k << " ";
+  //     }
+  //     cout << endl;
+  //   }
+  // }
+
+
 }
